1a21c8aa0cc0236ba8f9da669f2b818a538bfe77
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-06-15 02:17:19 -0400

    add runTick method to Board

--- src/App.tsx
+++ src/App.tsx
@@ -15,18 +15,18 @@ const App = () => {
   }
 
   class Board {
-    board: boolean[][];
+    matrix: boolean[][];
     constructor(height: number, length: number) {
-      this.board = [];
+      this.matrix = [];
       for (let row = 0; row < height; row++) {
         const newRow: boolean[] = new Array<boolean>(length).fill(false);
-        this.board.push(newRow);
+        this.matrix.push(newRow);
       }
     }
 
     toggleNode = (coordinates: [number, number]): void => {
       const [row, col] = coordinates;
-      this.board[row][col] = !this.board[row][col];
+      this.matrix[row][col] = !this.matrix[row][col];
     }
 
     evalNeighbors = (inputCoordinates: [number, number]): number => {
@@ -39,9 +39,9 @@ const App = () => {
             continue;
           }
           if (
-            this.board[currentRow] &&
-            this.board[currentRow][currentCol] &&
-            this.board[currentRow][currentCol] === true
+            this.matrix[currentRow] &&
+            this.matrix[currentRow][currentCol] &&
+            this.matrix[currentRow][currentCol] === true
             ) numOfAlive += 1;
         }
       }
@@ -50,35 +50,65 @@ const App = () => {
 
     evalNextNodeState = (inputCoordinates: [number, number]): boolean => {
       const [inputRow, inputCol] = inputCoordinates;
-      if (this.board[inputRow] === undefined || this.board[inputRow][inputCol] === undefined) {
+      if (this.matrix[inputRow] === undefined || this.matrix[inputRow][inputCol] === undefined) {
         throw Error('out-of-bounds');
       }
-      const currentIsAlive = this.board[inputRow][inputCol];
+      const currentIsAlive = this.matrix[inputRow][inputCol];
       const numNeighbors = this.evalNeighbors([inputRow, inputCol]);
       if (currentIsAlive) {
         if (numNeighbors === 2 || numNeighbors === 3) {
           return true;
         }
       } else {
-        if (numNeighbors === 3) return true;
+        if (numNeighbors === 3) {
+          return true;
+        }
       }
       return false;
+    }
+
+    runTick = (): void => {
+      const newMatrix: boolean[][] = [];
+
+      for (let row = 0; row < this.matrix.length; row++) {
+        const newRow: boolean[] = [];
+
+        for (let col = 0; col < this.matrix[row].length; col++) {
+          const coordinates: [number, number] = [row, col];
+          const outcome = this.evalNextNodeState(coordinates);
+          newRow.push(outcome);
+        }
+
+        newMatrix.push(newRow);
+      }
+
+      this.matrix = [...newMatrix];
     }
   }
 
 
+  /* TESTING */
   const board = new Board(5, 10);
   board.toggleNode([0, 1]);
-  board.toggleNode([0, 2]);
   board.toggleNode([1, 1]);
-  board.toggleNode([1, 2]);
+  board.toggleNode([2, 1]);
+  board.toggleNode([2, 2]);
+  board.toggleNode([1, 3]);
+  board.toggleNode([2, 7]);
+  board.toggleNode([3, 7]);
+  board.toggleNode([3, 8]);
   console.log(board.evalNeighbors([2, 3]) === 1);
   console.log(board.evalNeighbors([1, 1]) === 3);
   console.log(board.evalNeighbors([4, 6]) === 0);
   console.log(board.evalNextNodeState([2, 3]) === false);
   console.log(board.evalNextNodeState([1, 1]) === true);
   console.log(board.evalNextNodeState([4, 6]) === false);
-  console.log(board);
+  console.log(board.matrix);
+  board.runTick();
+  console.log(board.matrix);
+  board.runTick();
+  console.log(board.matrix);
+
 
   return (
     <div className="App">
