4ccc1709136ccc80e76e550873355e50d012b974
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-06-15 01:14:20 -0400

    install node-sass for scss compiler

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ APP Component | Tribute to Conway's Game of Life
 
 import React from 'react';
 
-import './App.css';
+import './App.scss';
 
 
 const App = () => {
   return (
     <div className="App">
-
+      Hello world! I am alive.
     </div>
   );
 }
