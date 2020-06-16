34eccc108704fa4b5fa8a2da8a7981fda382b1f8
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-06-16 15:18:38 -0400

    integrate functions evalNeighbors, evalNextNodeState, and runTick into App.js

--- src/App.tsx
+++ src/App.tsx
@@ -29,22 +29,63 @@ const App = () => {
   const [boolMatrix, setBoolMatrix] = useState(createMatrix(50, 50));
 
 
+  /* HELPER FUNCTIONS */
+  const evalNeighbors = (inputCoordinates: [number, number]): number => {
+    const [inputRow, inputCol] = inputCoordinates;
+    let numOfAlive: number = 0;
+    for (let row = -1; row <= 1; row++) {
+      for (let col = -1; col <= 1; col++) {
+        const [currentRow, currentCol] =  [inputRow + row, inputCol + col];
+        if (row === 0 && col === 0) {
+          continue;
+        }
+        if (
+          boolMatrix[currentRow] &&
+          boolMatrix[currentRow][currentCol] &&
+          boolMatrix[currentRow][currentCol] === true
+          ) numOfAlive += 1;
+      }
+    }
+    return numOfAlive;
+  }
 
+  const evalNextCellState = (inputCoordinates: [number, number]): boolean => {
+    const [inputRow, inputCol] = inputCoordinates;
+    if (boolMatrix[inputRow] === undefined || boolMatrix[inputRow][inputCol] === undefined) {
+      throw Error('out-of-bounds');
+    }
+    const currentIsAlive = boolMatrix[inputRow][inputCol];
+    const numNeighbors = evalNeighbors([inputRow, inputCol]);
+    if (currentIsAlive) {
+      if (numNeighbors === 2 || numNeighbors === 3) {
+        return true;
+      }
+    } else {
+      if (numNeighbors === 3) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-  // class Board {
-  //   matrix: boolean[][];
-  //   constructor(height: number, length: number) {
-  //     this.matrix = [];
-  //     for (let row = 0; row < height; row++) {
-  //       const newRow: boolean[] = new Array<boolean>(length).fill(false);
-  //       this.matrix.push(newRow);
-  //     }
-  //   }
+  const runTick = (): void => {
+    const newMatrix: boolean[][] = [];
 
-  // toggleNode = (coordinates: [number, number]): void => {
-  //   const [row, col] = coordinates;
-  //   this.matrix[row][col] = !this.matrix[row][col];
-  // }
+    for (let row = 0; row < boolMatrix.length; row++) {
+      const newRow: boolean[] = [];
+
+      for (let col = 0; col < boolMatrix[row].length; col++) {
+        const coordinates: [number, number] = [row, col];
+        const outcome = evalNextCellState(coordinates);
+        newRow.push(outcome);
+      }
+
+      newMatrix.push(newRow);
+    }
+
+    setBoolMatrix(newMatrix);
+  }
+
 
   const handleClickCell = (targetCoordinates: [number, number]) => {
     const [targetRow, targetCol] = targetCoordinates;
@@ -53,62 +94,7 @@ const App = () => {
     setBoolMatrix(newBoolMatrix);
   }
 
-  //   evalNeighbors = (inputCoordinates: [number, number]): number => {
-  //     const [inputRow, inputCol] = inputCoordinates;
-  //     let numOfAlive: number = 0;
-  //     for (let row = -1; row <= 1; row++) {
-  //       for (let col = -1; col <= 1; col++) {
-  //         const [currentRow, currentCol] =  [inputRow + row, inputCol + col];
-  //         if (row === 0 && col === 0) {
-  //           continue;
-  //         }
-  //         if (
-  //           this.matrix[currentRow] &&
-  //           this.matrix[currentRow][currentCol] &&
-  //           this.matrix[currentRow][currentCol] === true
-  //           ) numOfAlive += 1;
-  //       }
-  //     }
-  //     return numOfAlive;
-  //   }
 
-  //   evalNextNodeState = (inputCoordinates: [number, number]): boolean => {
-  //     const [inputRow, inputCol] = inputCoordinates;
-  //     if (this.matrix[inputRow] === undefined || this.matrix[inputRow][inputCol] === undefined) {
-  //       throw Error('out-of-bounds');
-  //     }
-  //     const currentIsAlive = this.matrix[inputRow][inputCol];
-  //     const numNeighbors = this.evalNeighbors([inputRow, inputCol]);
-  //     if (currentIsAlive) {
-  //       if (numNeighbors === 2 || numNeighbors === 3) {
-  //         return true;
-  //       }
-  //     } else {
-  //       if (numNeighbors === 3) {
-  //         return true;
-  //       }
-  //     }
-  //     return false;
-  //   }
-
-  //   runTick = (): void => {
-  //     const newMatrix: boolean[][] = [];
-
-  //     for (let row = 0; row < this.matrix.length; row++) {
-  //       const newRow: boolean[] = [];
-
-  //       for (let col = 0; col < this.matrix[row].length; col++) {
-  //         const coordinates: [number, number] = [row, col];
-  //         const outcome = this.evalNextNodeState(coordinates);
-  //         newRow.push(outcome);
-  //       }
-
-  //       newMatrix.push(newRow);
-  //     }
-
-  //     this.matrix = [...newMatrix];
-  //   }
-  // }
 
 
   /* TESTING */
