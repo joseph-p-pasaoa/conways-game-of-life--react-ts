843c1e83bdcd48ecb782e6ee439514570a1e1a92
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-06-19 02:26:53 -0400

    create memoized RowDisplay component

---
+++ src/components/RowDisplay.tsx
@@ -0,0 +1,51 @@
+/*
+JOSEPH P. PASAOA
+RowDisplay Component | Tribute to Conway's Game of Life
+*/
+
+
+
+/* IMPORT */
+import React, { memo } from 'react'
+
+import CellDisplay from './CellDisplay';
+
+
+
+/* TYPING */
+export interface Props {
+  boolRowIndex: number;
+  boolRow: boolean[];
+  handleClickCell(event: object): void;
+}
+
+
+
+/* COMPONENT */
+const MemoizedRowDisplay = memo(
+  function RowDisplay({ boolRowIndex, boolRow, handleClickCell }: Props) {
+    const displayRow = boolRow.map((isCellAlive, boolColIndex) => {
+        return(
+          <CellDisplay
+            key={boolRowIndex + boolColIndex}
+            isCellAlive={isCellAlive}
+            boolRowIndex={boolRowIndex}
+            boolColIndex={boolColIndex}
+            handleClickCell={handleClickCell}
+          />
+        );
+    })
+
+
+    return(
+      <div className='row'>
+        {displayRow}
+      </div>
+    );
+  }
+);
+
+
+
+/* EXPORT */
+export default MemoizedRowDisplay;
