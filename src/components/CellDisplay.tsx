6c4c9a1a4c0aff10604a4b07b397b59e5b58d520
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-06-19 02:27:20 -0400

    create memoized CellDisplay component

---
+++ src/components/CellDisplay.tsx
@@ -0,0 +1,44 @@
+/*
+JOSEPH P. PASAOA
+CellDisplay Component | Tribute to Conway's Game of Life
+*/
+
+
+
+/* IMPORT */
+import React, { memo } from 'react'
+
+
+
+/* TYPING */
+export interface Props {
+  isCellAlive: boolean;
+  boolRowIndex: number;
+  boolColIndex: number;
+  handleClickCell(event: object): void;
+}
+
+
+
+/* COMPONENT */
+const MemoizedCellDisplay = memo(
+  function CellDisplay({ isCellAlive, boolRowIndex, boolColIndex, handleClickCell }: Props) {
+    return(
+      <div
+        className={`cell${isCellAlive ? ' alive' : ''}`}
+        data-row={boolRowIndex}
+        data-col={boolColIndex}
+        onClick={(e) => {
+            const targetRow = e.currentTarget.dataset.row;
+            const targetCol = e.currentTarget.dataset.col;
+            handleClickCell([ targetRow, targetCol ]);
+        }}
+      ></div>
+    );
+  }
+);
+
+
+
+/* EXPORT */
+export default MemoizedCellDisplay;
