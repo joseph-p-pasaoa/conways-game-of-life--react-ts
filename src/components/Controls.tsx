4e4e1c1324ef39dea88c2c1ddb31bed94c156c6b
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-06-25 16:46:49 -0400

    elaborate label for tickDuration slider

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -34,6 +34,14 @@ const Controls = (props: Props) => {
   } = props;
 
 
+  let speedLabel = '1 sec (medium)';
+  if (tickDuration === 50) speedLabel = '1/20 sec (max)';
+  else if (tickDuration === 3000) speedLabel = '3 secs (min)';
+  else if (tickDuration < 900) speedLabel = 'fast';
+  else if (tickDuration > 1100) speedLabel = 'slow';
+  else if (tickDuration !== 1000) speedLabel = 'medium';
+
+
   return(
     <div className='controls'>
       <button
@@ -49,13 +57,14 @@ const Controls = (props: Props) => {
         Advance +1 Generation
       </button>
       <label htmlFor='inputTickDuration' className='tickduration__label'>
-        <div>Clock Speed</div>
+        <div>Clock Speed: {speedLabel}</div>
         <input
           id='inputTickDuration'
           type='range'
           value={tickDuration}
           min={50}
           max={3000}
+          step={50}
           onChange={handleChangeTickDuration}
           className='tickduration__rangeinput'
         />
