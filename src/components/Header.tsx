b559e781e38a40470cae2d07f1a9cdccac8bdd7e
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-08-03 03:34:37 -0400

    expand responsiveness to full-size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ import React from 'react'
 const Header = () => {
    return(
       <div className='header'>
-         <h1 className='title'>Conway's Game of Life:</h1>
+         <h1 className='title'>Conway's <br className='title__break-on-large' /> Game of Life:</h1>
          <h2 className='title--sub'>A Tribute Implementation</h2>
          <a href='https://github.com/joseph-p-pasaoa' target='_blank' rel='noopener noreferrer'>
             <h3 className='developer'>Joseph P. Pasaoa, Developer</h3>
